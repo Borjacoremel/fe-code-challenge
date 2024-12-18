1af8d10fccfd523bb1a8525e0ac12624d7d0e116
borjacore <bmirallesg@gmail.com>
2024-12-18 22:31:51 +0100

    feat: Performance optimisations
    - Code splitting by routes
    - Improve Vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,5 +6,49 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react(), tsconfigPaths()]
+  plugins: [
+    svgr(),
+    react({
+      fastRefresh: true
+    }),
+    ,
+    tsconfigPaths()
+  ],
+  build: {
+    target: 'esnext',
+    minify: 'esbuild',
+    sourcemap: false,
+    chunkSizeWarningLimit: 500,
+    cssCodeSplit: true,
+
+    reportCompressedSize: false,
+    emptyOutDir: true,
+
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor-react': ['react', 'react-dom', 'react-router-dom'],
+          'vendor-state': ['@reduxjs/toolkit', 'react-redux'],
+          'vendor-charts': ['recharts']
+        },
+
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]'
+      }
+    }
+  },
+  optimizeDeps: {
+    include: [
+      'react',
+      'react-dom',
+      'react-router-dom',
+      '@reduxjs/toolkit',
+      'react-redux',
+      'recharts'
+    ]
+  },
+  css: {
+    devSourcemap: true
+  }
 });
